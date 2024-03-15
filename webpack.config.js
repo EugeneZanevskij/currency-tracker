cdc8ed2b16867d67fe62f58980e8fb27ef1f5be9
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-03-15 15:24:30 +0300

    feat: setup eslint and prettier, update other setups

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './index.jsx',
-  mode: 'development',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-  },
-  target: 'web',
-  devServer: {
-    port: '3000',
-    static: {
-      directory: path.join(__dirname, 'public')
-    },
-    open: true,
-    hot: true,
-    liveReload: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/, 
-        exclude: /node_modules/, 
-        use: 'babel-loader', 
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html')
-    })
-  ]
+	entry: "./index.jsx",
+	mode: "development",
+	output: {
+		path: path.resolve(__dirname, "./dist"),
+		filename: "bundle.js",
+	},
+	resolve: {
+		extensions: [".js", ".jsx", ".json"],
+	},
+	target: "web",
+	devServer: {
+		port: "3000",
+		static: {
+			directory: path.join(__dirname, "public"),
+		},
+		open: true,
+		hot: true,
+		liveReload: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: "babel-loader",
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, "public", "index.html"),
+		}),
+	],
 };
