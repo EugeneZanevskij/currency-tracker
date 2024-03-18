c9a2f22a07fa88a47319a85039e27a3910e4b860
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-03-18 23:23:25 +0300

    feat: create routes for app

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import AppLayout from "../../layouts/AppLayout";
+import NAVBAR from "../../constants/nav";
+import ROUTES from "../../constants/routes";
+import NotFound from "../../pages/Notfound";
 
 function App() {
 	return (
 		<Routes>
 			<Route element={<AppLayout />}>
-				<Route path="/" element={<h1>Home</h1>} />
+				{Object.values(NAVBAR).map((item) => {
+					return (
+						<Route
+							key={item.link}
+							path={item.link}
+							element={<item.element />}
+						/>
+					);
+				})}
 			</Route>
+			<Route path={ROUTES.NOTFOUND} element={<NotFound />} />
 		</Routes>
 	);
 }
