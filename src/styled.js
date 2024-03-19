bc2d17365fd38ce80e44b9794482516bc174ed0a
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-03-19 23:24:32 +0300

    feat: create Global styles

--- src/styled.js
+++ src/styled.js
@@ -1,15 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 const Global = createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap');
 
-    body {
-      background-color: #f1f1f1;
-    }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #030304;
+    font-family: 'Poppins', sans-serif;
+  }
 `;
 
 export default Global;
