15dad268c28bab7c0aecbf40dacf3c188aff76cc
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-03-21 15:50:18 +0300

    style: change styling for CurrencySection

--- src/components/CurrencySection/styled.js
+++ src/components/CurrencySection/styled.js
@@ -4,9 +4,9 @@ const CurrencySectionContainer = styled.section`
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
+	gap: 4rem;
 	width: 85%;
-	margin: 0 auto;
-	gap: 1.5rem;
+	margin: 0 auto 6rem;
 `;
 
 export default CurrencySectionContainer;
