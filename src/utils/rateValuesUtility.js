dc8b3e151632b8a8df96332998ecd621fd34ab15
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-03-21 19:02:31 +0300

    feat: create getReversedValue function in utilities

--- src/utils/rateValuesUtility.js
+++ src/utils/rateValuesUtility.js
@@ -2,6 +2,10 @@ export function getValue(rates, id) {
 	return rates[id].value;
 }
 
+export function getReversedValue(rates, id) {
+	return 1 / getValue(rates, id);
+}
+
 export const getRateValue = (rates, name, rateValue) => {
 	if (rates && rates[name]) {
 		return `$ ${(1 / getValue(rates, name)).toFixed(3)}`;
