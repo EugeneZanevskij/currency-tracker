3c59209d93ebac15bb461c898d73d3638cf6cbab
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-03-21 15:46:59 +0300

    feat: add icons imports to currency constants

--- src/constants/currencies.js
+++ src/constants/currencies.js
@@ -1,13 +1,27 @@
+import ifix from "../assets/cards/IFIX.svg";
+import bovespa from "../assets/cards/BovespaIcon.svg";
+import usd from "../assets/cards/DollarIcon.svg";
+import ars from "../assets/cards/PesoArgentinoIcon.svg";
+import cad from "../assets/cards/CanadianDollarIcon.svg";
+import jpy from "../assets/cards/YenIcon.svg";
+import aud from "../assets/cards/AustralianDollarIcon.svg";
+import cny from "../assets/cards/WonIcon.svg";
+import eur from "../assets/cards/EuroIcon.svg";
+import bitcoin from "../assets/cards/BitcoinIcon.svg";
+import lbp from "../assets/cards/LibraIcon.svg";
+
 export const STOCKS_DATA = [
 	{
 		id: "IBOV",
 		title: "Bovespa Index",
 		rateValue: "0.15%",
+		img: bovespa,
 	},
 	{
 		id: "IFIX",
 		title: "IFIX",
 		rateValue: "0.15%",
+		img: ifix,
 	},
 ];
 
@@ -15,37 +29,46 @@ export const QUOTES_DATA = [
 	{
 		id: "USD",
 		title: "Commercial Dollar",
+		img: usd,
 	},
 	{
 		id: "ARS",
 		title: "Argentine Peso",
+		img: ars,
 	},
 	{
 		id: "CAD",
 		title: "Canadian Dollar",
+		img: cad,
 	},
 	{
 		id: "JPY",
 		title: "Yen",
+		img: jpy,
 	},
 	{
 		id: "AUD",
 		title: "Australian Dollar",
+		img: aud,
 	},
 	{
 		id: "CNY",
 		title: "Yuan",
+		img: cny,
 	},
 	{
 		id: "EUR",
 		title: "Euro",
+		img: eur,
 	},
 	{
 		id: "BTC",
 		title: "Bitcoin",
+		img: bitcoin,
 	},
 	{
 		id: "LBP",
 		title: "Libra",
+		img: lbp,
 	},
 ];
