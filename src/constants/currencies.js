e56e0e58ac8600cb0289ae565b0bb3df6e59b4c8
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-03-21 14:23:19 +0300

    feat: add stocks and quotes data to conctants

---
+++ src/constants/currencies.js
@@ -0,0 +1,49 @@
+export const STOCKS_DATA = [
+	{
+		title: "Bovespa Index",
+		rateValue: "0.15%",
+	},
+	{
+		title: "IFIX",
+		rateValue: "0.15%",
+	},
+];
+
+export const QUOTES_DATA = [
+	{
+		id: "USD",
+		title: "Commercial Dollar",
+	},
+	{
+		id: "ARS",
+		title: "Argentine Peso",
+	},
+	{
+		id: "CAD",
+		title: "Canadian Dollar",
+	},
+	{
+		id: "JPY",
+		title: "Yen",
+	},
+	{
+		id: "AUD",
+		title: "Australian Dollar",
+	},
+	{
+		id: "CNY",
+		title: "Yuan",
+	},
+	{
+		id: "EUR",
+		title: "Euro",
+	},
+	{
+		id: "BTC",
+		title: "Bitcoin",
+	},
+	{
+		id: "LBP",
+		title: "Libra",
+	},
+];
