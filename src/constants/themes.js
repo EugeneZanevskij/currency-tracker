8665453caec2ceae24bf5ccada466fc384e5b6f0
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-03-23 15:50:36 +0300

    feat: create colors constant

--- src/constants/themes.js
+++ src/constants/themes.js
@@ -2,14 +2,24 @@ import COLORS from "./styles/colors";
 
 const THEME = {
 	light: {
-		// ...BASE_THEME,
+		...COLORS,
 		backgroundColor: COLORS.white,
 		textColor: COLORS.black,
+		subTextColor: COLORS.darkGray,
+		cardColor: COLORS.lightGray,
+		cardTextColor: COLORS.cardGray,
+		percentsColor: COLORS.darkBlue,
+		modalColor: COLORS.lightGray,
 	},
 	dark: {
-		// ...BASE_THEME,
+		...COLORS,
 		backgroundColor: COLORS.black,
 		textColor: COLORS.white,
+		subTextColor: COLORS.suvaGray,
+		cardColor: COLORS.cardGray,
+		cardTextColor: COLORS.lightGray,
+		percentsColor: COLORS.lightBlue,
+		modalColor: COLORS.modalGray,
 	},
 };
 
