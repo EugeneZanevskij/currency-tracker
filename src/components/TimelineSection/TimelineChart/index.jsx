c2d1adf36be5f881ee020c4b61ba2ca7ceaeae78
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-03-27 02:15:48 +0300

    feat: add backgroundColors to chart

--- src/components/TimelineSection/TimelineChart/index.jsx
+++ src/components/TimelineSection/TimelineChart/index.jsx
@@ -70,21 +70,17 @@ class TimelineChart extends Component {
 							s: [2.0, 2.1],
 						},
 					],
-					backgroundColor: [
-						"rgba(255, 99, 132, 0.2)",
-						"rgba(54, 162, 235, 0.2)",
-						"rgba(255, 206, 86, 0.2)",
-						"rgba(75, 192, 192, 0.2)",
-						"rgba(153, 102, 255, 0.2)",
-					],
-					borderColor: [
-						"rgba(255, 99, 132, 1)",
-						"rgba(54, 162, 235, 1)",
-						"rgba(255, 206, 86, 1)",
-						"rgba(75, 192, 192, 1)",
-						"rgba(153, 102, 255, 1)",
-					],
-					borderWidth: 1,
+					backgroundColor: (context) => {
+						const {
+							raw: { o, c },
+						} = context;
+						if (c >= o) {
+							return "rgba(0, 255, 0, 0.5)";
+						}
+						return "rgba(255, 0, 0, 0.5)";
+					},
+					borderColor: "rgba(0, 0, 0, 1)",
+					borderWidth: 2,
 				},
 			],
 		};
