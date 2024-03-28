caaafc76c91be585934260fdee378440d8f8ffd6
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-03-28 11:15:06 +0300

    feat: create handleTimelineAdd function and pass it as prop to Modal

--- src/components/TimelineSection/index.tsx
+++ src/components/TimelineSection/index.tsx
@@ -4,6 +4,8 @@ import { TimelineCurrencySelect } from "@components/TimelineSection/TimelineCurr
 import { CURRENCY_DEFAULT } from "@constants/currencies";
 import TimelineCurrencyData from "./TimelineCurrencyData";
 import TimelineCurrencyModal from "./TimelineCurrencyModal";
+import { formatDate } from "@utils/timeUtilities";
+import { getCache, saveCache } from "@utils/cachingUtilities";
 
 export class TimelineSection extends Component {
 	state = {
@@ -22,6 +24,35 @@ export class TimelineSection extends Component {
 		this.setState({ isModalOpen: !this.state.isModalOpen });
 	};
 
+	handleTimelineAdd = (currentDate: Date, inputValue: IInputValue) => {
+		const formattedDate = formatDate(currentDate);
+		const { id } = this.state.selectedCurrency;
+
+		console.log(id, formattedDate, inputValue);
+
+		const currencyData: ICurrencyChartData = getCache(id) || {};
+
+		const high = Number(inputValue.highPriceInput);
+		const low = Number(inputValue.lowPriceInput);
+		const open = Number(inputValue.openPriceInput);
+		const close = Number(inputValue.closePriceInput);
+
+		if (high < open || high < low || high < close || low > open || low > close)
+			console.log("here");
+
+		console.log(currencyData);
+		currencyData[formattedDate] = {
+			open,
+			high,
+			low,
+			close,
+		};
+
+		console.log(currencyData[formattedDate]);
+
+		saveCache(id, currencyData);
+	};
+
 	render() {
 		const { selectedCurrency, isModalOpen, showChart } = this.state;
 		return (
@@ -37,6 +68,7 @@ export class TimelineSection extends Component {
 				{isModalOpen && (
 					<TimelineCurrencyModal
 						selectedCurrency={selectedCurrency}
+						onTimelineAdd={this.handleTimelineAdd}
 						onClose={this.handleModalToggle}
 					/>
 				)}
