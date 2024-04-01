93726a659e60113ef2fd5cc4453f638e8a8b5b28
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-04-01 22:08:42 +0300

    feat: create mapboxToken env and initialMap constant

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -4,6 +4,7 @@ const ENV_VARS = {
 	cacheThemeKey: String(process.env.CACHE_THEME_KEY),
 	currencyApiKey: String(process.env.CURRENCY_API_KEY),
 	currencyApiUrl: String(process.env.CURRENCY_API_URL),
+	mapboxToken: String(process.env.MAPBOX_TOKEN),
 };
 
 export const getEnvVars = (key: keyof typeof ENV_VARS) => {
---
+++ src/constants/map.ts
@@ -0,0 +1,6 @@
+export const INITIAL_MAP = {
+	center: [23.799, 53.671],
+	style: "mapbox://styles/mapbox/streets-v11",
+	zoom: 11,
+	attributionControl: false,
+};
